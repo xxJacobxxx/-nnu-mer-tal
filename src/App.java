e48eb1058040dc244af69799d6326f5f08d288b4
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-04 09:34:31 +0200

    Klar med uppgiften för jag glömde

---
+++ src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    int tal1 = 23;   
+    int tal2 = 45;
+    int summa;
+    int produkt;
+    summa = tal1+tal2;
+    System.out.println(tal1+ "+"+tal2+"="+summa);
+    summa = tal2-tal1;
+    System.out.println(tal2+ "-"+tal1+"="+summa);
+    
+    int tal3 = 234232;
+    int tal4 = 3424;
+    produkt= tal2*tal3*tal4;
+    System.out.println(produkt);
+    produkt = tal1/tal2;
+    int tal5=10;
+    int tal6=3;
+    double kvot;
+    kvot= tal6/tal3;
+    System.out.println(kvot);
+    int modul1= tal5%tal6;
+    int modul2 = 12%4;
+    System.out.println(modul1+" "+modul2);
+
+    }
+}
